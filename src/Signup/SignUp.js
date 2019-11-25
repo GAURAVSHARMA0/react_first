26d4a412fe12f820acf4f16ad9426c5aca14c5ed
GAURAVSHARMA0 <48209599+GAURAVSHARMA0@users.noreply.github.com>
2019-11-25 12:58:35 +0530

    use meterial ui

--- src/Signup/SignUp.js
+++ src/Signup/SignUp.js
@@ -1,66 +1,79 @@
-import React from 'react';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+//import { makeStyles } from "@material-ui/core/styles";
 
-class SignUp extends React.Component{
-    constructor(){
-        super();
-        this.state ={
-            name :"",
-            age: "",
-            fathername: "",
-            password:""
-        }
-    }
+class SignUp extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: "",
+      fathername: "",
+      password: ""
+    };
+  }
 
-    onChangeHandler =(event) =>{
-        this.setState({
-            [event.target.name] : event.target.value
-        })
-    }
+  onChangeHandler = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-    SubmitHandler = (e) =>{
-        e.preventDefault();
-        console.log(this.state);
-        // const data = new FormData(e.target);
-        // fetch('',{
-        //     method:'GET',
-        //     body:data,
-        // })
+  SubmitHandler = e => {
+    e.preventDefault();
+    alert(JSON.stringify(this.state));
+    console.log(this.state);
+    // const data = new FormData(e.target);
+    // fetch('',{
+    //     method:'GET',
+    //     body:data,
+    // })
+  };
 
-    }
-    
-    render(){
-        const style ={
-                backgroundColor: '#2e6da4',
-                border: 'none',
-                color: 'white',
-                padding: '12px 25px',
-                textAlign: 'center',
-                textDecoration: 'none',
-                display: 'inline-block',
-                fontSize: '16px',
-                margin: '16px 2px',
-                borderRadius: '4px',
-                cursor: 'pointer'
-
-        }
-    
-
-        return(
-        <form onSubmit={this.SubmitHandler}>
-            <div>
-                <p>Enter your name</p>
-                <input type="text" name="name" onChange={this.onChangeHandler}/>
-                <p>Enter your age is</p>
-                <input type="text" name="age" onChange={this.onChangeHandler} />
-                <p>Enter your father's name</p>
-                <input type="text" name="fathername" onChange={this.onChangeHandler} />
-                <p>Enter Password</p>
-                <input type="password" name="password" onChange={this.onChangeHandler} /><br/>
-                <button style={style}>SUBMIT</button>
-            </div>
-        </form>
-        );
-    }
+  render() {
+    return (
+      <form noValidate autoComplete="off" onSubmit={this.SubmitHandler}>
+        <div>
+          <p>Enter your name</p>
+          <TextField
+            id="outlined-input"
+            label="Name"
+            variant="outlined"
+            onChange={this.onChangeHandler}
+          />
+          <p>Enter your age is</p>
+          <TextField
+            id="outlined-input"
+            label="Age"
+            variant="outlined"
+            name="age"
+            onChange={this.onChangeHandler}
+          />
+          <p>Enter your father's name</p>
+          <TextField
+            id="outlined-input"
+            label="Father's name"
+            variant="outlined"
+            onChange={this.onChangeHandler}
+          />
+          <p>Enter Password</p>
+          <TextField
+            id="outlined-password-input"
+            type="password"
+            label="Password"
+            variant="outlined"
+            onChange={this.onChangeHandler}
+          />
+          <br />
+          <br />
+          <Button variant="contained" color="primary">
+            SUBMIT
+          </Button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default SignUp;
